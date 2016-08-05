b776323d3fd6bc5fa3fb70c78229a0d45c9f628c
Aurelien Chevallier <aurelien.chevallier@free.fr>
2016-08-05 12:21:06 +0200

    add new theme module "SuperHero" from Bootswatch

---
+++ website_theme_superhero/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Bootstrap theme Superhero',
+    'category': 'Website',
+    'summary': 'Theme from Bootswatch',
+    'website': 'https://github.com/ach-odoo/theme_bootstrap',
+    'version': '1.0',
+    'description': """
+Superhero theme from Bootswatch
+===============================
+
+You can download original css at http://bootswatch.com/2/superhero/
+
+        """,
+    'author': 'Aurelien Chevallier',
+    'depends': ['website'],
+    'installable': True,
+    'data': [
+        'view/theme.xml',
+    ],
+    'demo': [],
+    'qweb':[], 
+    'application': True,
+}
